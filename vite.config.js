b25f166c4b4f75b8971072c54ef9f041bc54b8d5
Arnau Nin <ninbsnss92@gmail.com>
2025-03-20 13:58:14 +0100

    Building project

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: "./", // Usa rutas relativas en los enlaces generados en el HTML
+  base: "/Pair-colors-game/", // Usa rutas relativas en los enlaces generados en el HTML
   server: {
     watch: {
       usePolling: true, // Usa polling para detectar cambios
